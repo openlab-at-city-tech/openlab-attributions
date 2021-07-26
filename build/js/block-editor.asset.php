b5c9a34f0807001e96e4f5f1ca8a288df62ae098
Boone B Gorges <boonebgorges@gmail.com>
2021-07-26 13:51:07 -0500

    Add build files.

---
+++ build/js/block-editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-rich-text'), 'version' => '23bd3e85340e9c7c80f81afa19fb5926');
---
+++ build/js/classic-editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '458049c1f5b4be0d68f1ae4131ef8a4b');
