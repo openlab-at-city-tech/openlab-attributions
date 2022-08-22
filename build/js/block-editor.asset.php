8db847a603fab25f9774e1abde6474ddba14856b
Boone B Gorges <boonebgorges@gmail.com>
2022-08-22 10:30:26 -0500

    Add build files for 2.0.1 release.

---
+++ build/js/block-editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-rich-text'), 'version' => 'ef01e6794f0d43cb');
---
+++ build/js/classic-editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'f8deb3a9f04365c9');
