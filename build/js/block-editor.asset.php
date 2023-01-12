95d2e1e11a11c482fb92208c17419cd436cf7404
Boone B Gorges <boonebgorges@gmail.com>
2023-01-12 14:59:35 -0600

    Build for 2.1.1

---
+++ build/js/block-editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-rich-text'), 'version' => '49429a7714d801b84cfef5588a0cba91');
